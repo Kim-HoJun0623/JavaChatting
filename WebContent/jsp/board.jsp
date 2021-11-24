38a3773d120cc945a01f06b5797ee1a1d207c5f7
김호준 <rlaghwns126@naver.com>
2021-11-24 18:58:56 +0900

    친구리스트 생성

---
+++ WebContent/jsp/board.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ <%@ page import="catting.BoardDAO" %>
+<%@ page import="user.UserVO" %>
+<%@ page import="java.util.ArrayList" %>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/boardstyle.css" />
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<table class="type11">
+  <thead>
+  <tr>
+    <th scope="cols">아이디</th>
+    <th scope="cols">이름</th>
+    <th scope="cols">번호</th>
+  </tr>
+  </thead>
+  <tbody>
+  <%
+					BoardDAO boardDAO = new BoardDAO();
+				ArrayList<UserVO> list = boardDAO.friendlist();
+					for(int i=0; i<list.size(); i++){
+				%>
+  			<tr>
+ 		
+			
+			<td><%= list.get(i).getId() %></th>
+    		<td><%= list.get(i).getName() %></th>
+    		<td><%=list.get(i).getPhone1()+"-"+list.get(i).getPhone2()+"-"+list.get(i).getPhone3() %></th>
+			
+				 
+				
+  </tr>
+  	<%
+								}
+						%>
+
+  </tbody>
+
+					
+</body>
+</html>
--- src/catting/BoardDAO.java
+++ src/catting/BoardDAO.java
@@ -1,8 +1,10 @@
 package catting;
 
+import user.UserVO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -14,31 +16,47 @@ public class BoardDAO {
     private DataSource dataFactory;
     
     public BoardDAO() {
+    	System.out.println("오류검출DB");
     	 try {
              Context ctx = new InitialContext();
              Context envContext = (Context) ctx.lookup("java:/comp/env");
              dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
           } catch (Exception e) {
+        	
              e.printStackTrace();
           }
     }
-    public void friendlist() {
+    public  ArrayList<UserVO> friendlist() {
+		
+        String query = "select * from chat_members";
+        ArrayList<UserVO> list = new ArrayList<UserVO>();
     	try {
     		con = dataFactory.getConnection();
-            String query = "select id from chat_memever";
-            pstmt = con.prepareStatement(query);
+    		pstmt = con.prepareStatement(query);
             ResultSet rs = pstmt.executeQuery();
+    
             while(rs.next()) {
-            	String id = rs.getString("id");
+ /*           	String id = rs.getString("id");
             	String pw = rs.getString("pw");
             	String name=rs.getString("name");
             	String phone1 =rs.getString("phone1");
             	String phone2 =rs.getString("phone2");
-            	String phone3 =rs.getString("phone3");
+            	String phone3 =rs.getString("phone3");*/
             
+            	UserVO vo = new UserVO();
+            	vo.setId(rs.getString(1));
+            	vo.setPw(rs.getString(2));
+            	vo.setName(rs.getString(3));
+            	vo.setPhone1(rs.getString(4));
+            	vo.setPhone2(rs.getString(5));
+            	vo.setPhone3(rs.getString(6));
+            	list.add(vo);
+            	
             }
     	}catch(Exception e) {
+    	
     		e.printStackTrace();
     	}
+    	return list;
     }
 }
